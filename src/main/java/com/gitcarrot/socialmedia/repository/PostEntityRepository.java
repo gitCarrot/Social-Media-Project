0ef063674c2e9473cd0df35358fb5e438eab35fe
gitCarrot <jeonhg0@gmail.com>
2023-04-24 06:27:14 +0900

    Pageable Function for Feed

--- src/main/java/com/gitcarrot/socialmedia/repository/PostEntityRepository.java
+++ src/main/java/com/gitcarrot/socialmedia/repository/PostEntityRepository.java
@@ -1,9 +1,14 @@
 package com.gitcarrot.socialmedia.repository;
 
 import com.gitcarrot.socialmedia.model.entity.PostEntity;
+import com.gitcarrot.socialmedia.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+
+    Page<PostEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
 }
