b3f98e9c1c415297d56cbddc4f5b7fd72da3f074
gitCarrot <jeonhg0@gmail.com>
2023-04-24 06:27:39 +0900

    Update PostController.java

--- src/main/java/com/gitcarrot/socialmedia/controller/PostController.java
+++ src/main/java/com/gitcarrot/socialmedia/controller/PostController.java
@@ -8,6 +8,8 @@ import com.gitcarrot.socialmedia.controller.response.Response;
 import com.gitcarrot.socialmedia.model.Post;
 import com.gitcarrot.socialmedia.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +33,28 @@ public class PostController {
         return Response.success(PostResponse.fromPost(post));
     }
 
+<<<<<<< Updated upstream
+=======
+    @DeleteMapping("/{postId}")
+    public Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
+        postService.delete(authentication.getName(), postId);
+        return Response.success();
+    }
+
+    @GetMapping
+    public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication){
+        return Response.success(postService.list(pageable).map(PostResponse::fromPost));
+    }
+
+
+    @GetMapping("/my")
+    public Response<Page<PostResponse>> my(Pageable pageable, Authentication authentication){
+        return Response.success(postService.my(authentication.getName(), pageable).map(PostResponse::fromPost));
+    }
+
+
+
+>>>>>>> Stashed changes
 
 
 }
